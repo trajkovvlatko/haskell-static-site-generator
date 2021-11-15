d82779648bfe4239fe9a9240360a4c7f2cf801cb
Vlatko Trajkov <trajkovvlatko@gmail.com>
2021-11-15 17:00:48 +0100

    Add args parser

--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -1,52 +1,25 @@
 {-# LANGUAGE LambdaCase #-}
 
-module HsBlog where
-( main
-, process
-)
+module HsBlog
+  ( convertSingle
+  , convertDirectory
+  , process
+  )
 where
 
 import qualified HsBlog.Markup as Markup
 import qualified HsBlog.Html as Html
 import HsBlog.Convert (convert)
 
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
+import System.IO
 
-main :: IO ()
-main =
-  getArgs >>= \case
-    [] -> do
-      content <- getContents
-      let html = process "No title" content
-      putStrLn html
+convertSingle :: Html.Title -> Handle -> Handle -> IO ()
+convertSingle title input output = do
+  content <- hGetContents input
+  hPutStrLn output (process title content)
 
-    [input, output] -> do
-      content <- readFile input
-      exists <- doesFileExist output
-      let html = process input content
-          writeResult = writeFile output html
-      if exists
-        then whenIO confirm writeResult
-        else writeResult
-
-    _ -> putStrLn "Usage: runhaskell Main.hs <input> <output>"
+convertDirectory :: FilePath -> FilePath -> IO ()
+convertDirectory = error "Not implemented"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
-
-confirm :: IO Bool
-confirm = do
-  putStrLn "Are you sure? (y/n)"
-  answer <- getLine
-  case answer of
-    "y" -> pure True
-    "n" -> pure False
-    _ -> putStrLn "Invalid response. use y or n" *> confirm
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = do
-  result <- cond 
-  if result
-    then action
-    else pure ()
